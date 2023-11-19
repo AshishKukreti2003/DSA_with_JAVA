fb3530d58a8895eee0bcd65299af419b03f46dcb
AshishKukreti2003 <ashishkukreti4546@gmail.com>
2023-11-19 12:52:59 +0530

    Java Inputs

---
+++ First_Java_Program_Input_Output,_Debugging_and_Datatypes/lecture/Inputs.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Inputs {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // System.out.print("Enter the Rollno. ");
+        // int rollno = sc.nextInt();
+        // System.out.println("Roll number is: " + rollno);
+
+        // System.out.print("Enter your marks: ");
+        // float marks = sc.nextFloat();
+        // System.out.println("Your's marks is: "+ marks);
+
+        System.out.print("Enter your Name: ");
+        String name = sc.nextLine();
+        System.out.println("Name is: "+ name);
+
+        sc.close();
+    }
+}
