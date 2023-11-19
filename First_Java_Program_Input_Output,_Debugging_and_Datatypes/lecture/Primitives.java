0ef1a1608938d45ffc1b3ff379e2fb19e338cde5
AshishKukreti2003 <ashishkukreti4546@gmail.com>
2023-11-19 12:42:55 +0530

    Java Primitives

---
+++ First_Java_Program_Input_Output,_Debugging_and_Datatypes/lecture/Primitives.java
@@ -0,0 +1,17 @@
+public class Primitives {
+    public static void main(String[] args){
+        int rollno = 456;
+        float marks = 89.98f;
+        char letter = 'a';
+        double largeDecimalNumber = 435656.43567453;
+        long largeIntegerNUmber = 45678987654567L;
+        boolean check = false;
+        System.out.println(rollno);
+        System.out.println(marks);
+        System.out.println(letter);
+        System.out.println(largeDecimalNumber);
+        System.out.println(largeIntegerNUmber);
+        System.out.println(check);
+    }
+    
+}
