53f7eb40f7ba02d49a96cf6b1cf09736010ac611
AshishKukreti2003 <ashishkukreti4546@gmail.com>
2023-11-20 10:24:05 +0530

    practice question solved

--- First_Java_Program_Input_Output,_Debugging_and_Datatypes/practice/Practice.java
+++ First_Java_Program_Input_Output,_Debugging_and_Datatypes/practice/Practice.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Practice {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        // 1. Write a program to print whether a number is even or odd, also take input
+// 1. Write a program to print whether a number is even or odd, also take input
         // from the user.
         // System.out.print("Enter the number: ");
         // int a = sc.nextInt();
@@ -12,12 +12,12 @@ public class Practice {
         // } else
         //     System.out.println("Number is Odd");
 
-        // 2. Take name as input and print a greeting message for that particular name.
+// 2. Take name as input and print a greeting message for that particular name.
         // System.out.print("Enter the Name: ");
         // String name = sc.next();
         // System.out.println("Good Morning " + name);
 
-        // 3. Write a program to input principal, time, and rate (P, T, R) from the user
+ // 3. Write a program to input principal, time, and rate (P, T, R) from the user
         // and find Simple Interest.
         /* SI = (P*R*T)/100 */
         // System.out.print("Enter the principal amount: ");
@@ -30,7 +30,7 @@ public class Practice {
         // double SI = (prin * rate * time) / 100;
         // System.out.println("The Simple Interest is: " + SI);
 
-        // 4. Take in two numbers and an operator (+, -, *, /) and calculate the value.
+// 4. Take in two numbers and an operator (+, -, *, /) and calculate the value.
         // System.out.print("Enter the 1st number: ");
         // int num1 = sc.nextInt();
         // System.out.print("Enter the 2nd number: ");
@@ -40,7 +40,7 @@ public class Practice {
         // System.out.println("The * is: " + (num1 * num2));
         // System.out.println("The / is: " + (num1 / num2));
 
-        // 5. Take 2 numbers as input and print the largest number.
+// 5. Take 2 numbers as input and print the largest number.
         // System.out.print("Enter the 1st number: ");
         // int x = sc.nextInt();
         // System.out.print("Enter the 2nd number: ");
@@ -50,14 +50,14 @@ public class Practice {
         // } else
         //     System.out.println("Largest number is: " + y);
 
-        // 6. Input currency in rupees and output in USD.
+// 6. Input currency in rupees and output in USD.
         // System.out.print("Enter the rupees: ");
         // double rupee = sc.nextDouble();
         // double usd = rupee / 85;
         // System.out.println("USD of " + rupee + "rupees is $" + usd);
 
-        // 7. To calculate Fibonacci Series up to n numbers.
-        // System.out.println("Enter the number: ");
+// 7. To calculate Fibonacci Series up to n numbers.
+        // System.out.print("Enter the number: ");
         // int op = sc.nextInt();
         // int temp = 0;
         // int e = 0, f = 1;
@@ -68,22 +68,37 @@ public class Practice {
         //     f = temp;
         // }
 
-        // 8. To find out whether the given String is Palindrome or not.
-        System.out.println("Enter the String: ");
-        String str = sc.nextLine();
-        String org_str = str;
-        String rev ="";
-        for(int i=str.length()-1;i>=0;i--){
-            rev+=str.charAt(i);
-        }
-        // System.out.println(rev); // reverse a String
-        if(org_str.equals(rev)){
-            System.out.println(org_str+ " is a Palindrome String");
-        }
-        else
-            System.out.println(org_str+ " is not a Palindrome String");
+// 8. To find out whether the given String is Palindrome or not.
+        // System.out.print("Enter the String: ");
+        // String str = sc.nextLine();
+        // String org_str = str;
+        // String rev ="";
+        // for(int i=str.length()-1;i>=0;i--){
+        //     rev+=str.charAt(i);
+        // }
+        // // System.out.println(rev); // reverse a String
+        // if(org_str.equals(rev)){
+        //     System.out.println(org_str+ " is a Palindrome String");
+        // }
+        // else
+        //     System.out.println(org_str+ " is not a Palindrome String");
 
-        // 9. To find Armstrong Number between two given number.
+// 9. To find Armstrong Number between two given number.
+        System.out.print("Enter the first number: ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter the second number: ");
+        int num2 = sc.nextInt();
+        for(int i=num1;i<num2;i++){
+            int check = i, rem, sum=0;
+            while(check != 0){
+                rem = check%10;
+                sum += rem*rem*rem;
+                check = check/10;
+            }
+            if(sum ==i){
+                System.out.println(i+" is a Armstrong number");
+            }
+        }
 
         sc.close();
     }
