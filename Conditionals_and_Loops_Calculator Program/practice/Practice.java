1e65a295e8e9c39941a25e49642b69be25515a48
AshishKukreti2003 <ashishkukreti4546@gmail.com>
2023-11-20 20:09:48 +0530

    practice question solved

---
+++ Conditionals_and_Loops_Calculator Program/practice/Practice.java
@@ -0,0 +1,97 @@
+import java.util.*;
+
+public class Practice {
+    public static void main(String[] args){
+        
+// 1. Area Of Circle Java Program
+        // int radius_of_circle = 5;
+        // double area_of_circle = Math.PI*radius_of_circle*radius_of_circle;
+        // System.out.println("Area of Circle is: "+ area_of_circle);
+
+// 2. Area Of Triangle
+        // int base_of_triangle = 5;
+        // int height_of_triangle = 7;
+        // int area_of_triangle = (base_of_triangle*height_of_triangle)/2;
+        // System.out.println("Area of the triangle is: "+ area_of_triangle);
+
+// 3. Area Of Rectangle Program 
+        // int length_of_rect = 5;
+        // int width_of_rect = 3;
+        // int area_of_rect = length_of_rect*width_of_rect;
+        // System.out.println("Area of the Rectangle is: "+ area_of_rect);
+
+// 4. Area Of Isosceles Triangle 
+        // int base_of_triangle = 5;
+        // int height_of_triangle = 7;
+        // int area_of_isosceles_triangle = (base_of_triangle*height_of_triangle)/2;
+        // System.out.println("Area of the Isosceles triangle is: "+ area_of_isosceles_triangle);
+
+// 5. Area Of Parallelogram
+        // int base_of_parallelogram = 6;
+        // int height_of_parallelogram = 14;
+        // int area_of_parallelogram = base_of_parallelogram* height_of_parallelogram;
+        // System.out.println("Area of the parallelogram is: "+ area_of_parallelogram);
+
+// 6. Area Of Rhombus
+        // int diagonal1_of_rhombus = 9;
+        // int diagonal2_of_rhombus = 5;
+        // int area_of_rhombus = (diagonal1_of_rhombus*diagonal2_of_rhombus)/2;
+        // System.out.println("Area of Rhombus is: "+ area_of_rhombus);
+
+// 7. Area Of Equilateral Triangle
+        // int side_of_triangle = 6;
+        // double area_of_equilateral_triangle= (Math.sqrt(3)*side_of_triangle*side_of_triangle)/4;
+        // System.out.println("Ares of Equilateral Triangle is: "+ area_of_equilateral_triangle );
+
+// 8. Perimeter Of Circle
+        // int radius_of_circle = 5;
+        // double Perimeter_of_circle = 2*Math.PI*radius_of_circle;
+        // System.out.println("Perimeter of Circle is: "+ Perimeter_of_circle);
+
+// 9. Perimeter Of Equilateral Triangle
+        // int side= 9;
+        // int Perimeter_of_Equilateral_Triangle = 3*side;
+        // System.out.println("Perimeter Of Equilateral Triangle is: "+ Perimeter_of_Equilateral_Triangle);
+
+// 10. Perimeter Of Parallelogram
+        // int side1 = 56;
+        // int side2 = 67;
+        // int Perimeter_Of_Parallelogram = 2*(side1+side2);
+        // System.out.println("Perimeter Of Parallelogram is: "+ Perimeter_Of_Parallelogram);
+
+// 11. Perimeter Of Rectangle
+        // int length = 5;
+        // int width = 7;
+        // int Perimeter_Of_Rectangle = 2*(length+width);
+        // System.out.println("Perimeter Of Parallelogram is: "+ Perimeter_Of_Rectangle);
+
+// 12. Perimeter Of Square
+        // int side = 5;
+        // int perimeter_of_square = 4*side;
+        // System.out.println("Perimeter of Square is: "+ perimeter_of_square); 
+
+// 13. Perimeter Of Rhombus
+        // int side = 9;
+        // int perimeter_of_rhombus = 4*side;
+        // System.out.println("Perimeter of Square is: "+ perimeter_of_rhombus); 
+
+// 14. Volume Of Cone Java Program
+        // int radius = 7;
+        // int height = 9;
+        // double volume_of_cone = (Math.PI*radius*radius*height)/3;
+        // System.out.println("Volume of Cone is: "+ volume_of_cone);
+
+// 15. Volume Of Prism
+        
+
+
+    }
+}
+/*
+ * 16. Volume Of Cylinder
+ * 17. Volume Of Sphere
+ * 18. Volume Of Pyramid
+ * 19. Curved Surface Area Of Cylinder
+ * 20. Total Surface Area Of Cube
+ * 21. Fibonacci Series In Java Programs
+ */
