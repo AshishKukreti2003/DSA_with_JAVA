dcce43a33ab960569f0b0600790b38814332dfc1
AshishKukreti2003 <ashishkukreti4546@gmail.com>
2023-11-21 09:03:00 +0530

    Conditionals in Java

---
+++ Conditionals_and_Loops_Calculator Program/lecture/Conditionals.java
@@ -0,0 +1,26 @@
+
+public class Cpnditionals {
+    public static void main(String[] args){
+        int salary = 20000;
+
+        if(salary>10000){
+            salary+=5000;
+        }
+        else{
+            salary+=1000;
+        }
+        System.out.println(salary);
+
+        // multiple if-else
+        int money = 10000;
+        if(money<5000){
+            money+=1000;
+        }
+        else if (money>7000) {
+            money+=2000;
+        }
+        else
+            money+=500;
+        System.out.println(money);
+    }
+}
