84bb0e4911e09a88b94310b6343881f1e6e098e8
AshishKukreti2003 <ashishkukreti4546@gmail.com>
2023-11-21 09:34:31 +0530

    Loops in Java

---
+++ Conditionals_and_Loops_Calculator Program/lecture/Loops.java
@@ -0,0 +1,25 @@
+public class Loops {
+    public static void main(String[] args){
+        // print number form 1 to 5
+        // for loops
+        for(int i=1;i<=5;i++){
+            System.out.println(i);
+        }
+
+        // while loops
+        int a = 1;
+        while(a!=6){
+            System.out.println(a);
+            a++;
+        }
+
+        // do-while loop
+        int num = 1;
+        do{
+            System.out.println(num);
+            num++;
+        }while(num<=5);
+
+    
+    }
+}
