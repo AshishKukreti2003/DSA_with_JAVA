ca6b2457d370b603ebac8248c0a5968ca5551e63
AshishKukreti2003 <ashishkukreti4546@gmail.com>
2023-11-23 20:28:22 +0530

    Conditionals and Loops Question Solved

--- Conditionals_and_Loops_Calculator Program/lecture/Question.java
+++ Conditionals_and_Loops_Calculator Program/lecture/Question.java
@@ -23,15 +23,40 @@ public class Question {
         // System.out.println("Maximum number is: "+ max);
 
 // Q2. Character Case check where it is UPPERCASE or lowercase
-        char ch = sc.next().trim().charAt(0);
-        if(ch >= 'a' && ch <= 'z'){
-            System.out.println("lowercase");
-        }
-        else
-            System.out.println("Uppercase");
+        // char ch = sc.next().trim().charAt(0);
+        // if(ch >= 'a' && ch <= 'z'){
+        //     System.out.println("lowercase");
+        // }
+        // else
+        //     System.out.println("Uppercase");
         
+// Q3. Calculate how many time integer n occuring in a number
+        // System.out.print("Enter the number: ");
+        // int num = sc.nextInt();
+        // System.out.print("Enter the integer: ");
+        // int integer = sc.nextInt();
+        // int count=0;
+        // while(num>0){
+        //    int rem;
+        //    rem = num%10;
+        //    if(rem==integer){
+        //         count++;
+        //     }
+        //     num=num/10;
+        // }
+        // System.out.println(count);
 
-
+// Q4. Reverse a number
+        System.out.print("Enter the number: ");
+        int num = sc.nextInt();
+        int ans =0;;
+        while(num>0){
+            int rem = num%10;
+            num /=10;
+            ans = ans*10+rem;
+        }
+        System.out.println("Reverse a number: " + ans);
+        
         sc.close();
     }
 }
