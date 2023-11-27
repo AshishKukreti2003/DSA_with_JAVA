4784b15f9e3d06605a938a45fb554ae3ee3490ad
AshishKukreti2003 <ashishkukreti4546@gmail.com>
2023-11-27 22:53:35 +0530

    File Modified

--- Conditionals_and_Loops_Calculator Program/practice/Intermediate_practice.java
+++ Conditionals_and_Loops_Calculator Program/practice/Intermediate_practice.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Intermediate_practice {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        // 1. Factorial Program In Java
+// 1. Factorial Program In Java
         // int num = 10;
         // int result =1;
         // int count =1;
@@ -13,7 +13,7 @@ public class Intermediate_practice {
         // }
         // System.out.println(result);
 
-        // 2. Calculate Average Of N Numbers
+// 2. Calculate Average Of N Numbers
         // double num = sc.nextDouble();
         // double sum =0;
         // int count =0;
@@ -25,14 +25,14 @@ public class Intermediate_practice {
         // double avg = sum/num;
         // System.out.println(avg);
 
-        // 3. Calculate Discount Of Product
+// 3. Calculate Discount Of Product
         // double originalPrice = 100.0; // Original price of the product
         // double discountPercentage = 40.0; // Discount percentage
 
         // double discount = originalPrice * (discountPercentage / 100);
         // System.out.println("The discount on the product is: $" + discount);
 
-        // 4. Calculate Distance Between Two Points
+// 4. Calculate Distance Between Two Points
         // int point1 = 100;
         // int point2 = 120;
         // int Distance;
@@ -44,19 +44,19 @@ public class Intermediate_practice {
         // System.out.println("The Distance Between "+ point1 +" and "+ point2+" is: "+
         // Distance);
 
-        // 5. Power In Java
+// 5. Power In Java
         // int num = 2;
         // int exponent = 10;
         // double power = Math.pow(num, exponent);
         // System.out.println(power);
 
-        // 6. Calculate Batting Average
+// 6. Calculate Batting Average
         // int balls = 2000;
         // int out = 8;
         // double ba = balls/out;
         // System.out.println(ba);
 
-        // 7. Compound Interest Java Program
+// 7. Compound Interest Java Program
         // int initial_amount = 70000;
         // double rate = 0.0710;
         // int timesCompounded = 100;
@@ -67,7 +67,7 @@ public class Intermediate_practice {
         // System.out.println(ci);
         // System.out.println(amount);
 
-        // 8. Sum Of N Numbers
+// 8. Sum Of N Numbers
         // int n = sc.nextInt();
         // int sum = 0;
         // int count = 1;
@@ -78,28 +78,37 @@ public class Intermediate_practice {
         // }
         // System.out.println(sum);
 
-        // 9. Armstrong Number In Java
-        int num = sc.nextInt();
-        int result = num;
+// 9. Armstrong Number In Java
+        // int num = sc.nextInt();
+        // int result = num;
         // int length = String.valueOf(num).length();
-        // System.out.println(length);
-        int sum = 0;
-        while (num>0) {
-            int digit = num%10;
-            sum+=digit*digit*digit;
-            num /= 10;
-        }
-        if(sum == result){
-            System.out.println(result+" is a Armstrong number");
-        }
-        else
-            System.out.println(result+" is not a Armstrong number");
+        // // System.out.println(length);
+        // int sum = 0;
+        // while (num>0) {
+        //     int digit = num%10;
+        //     sum += Math.pow(digit, length);
+        //     num /= 10;
+        // }
+        // if(sum == result){
+        //     System.out.println(result+" is a Armstrong number");
+        // }
+        // else
+        //     System.out.println(result+" is not a Armstrong number");
+
+//  10. Find Ncr & Npr
+        System.out.print("Enter the value of n: ");
+        int n = sc.nextInt();
+        System.out.print("Enter the value of r: ");
+        int r = sc.nextInt();
+
+
+
+
+        sc.close();
     }
 }
 
 /*
- * 
- * 15. Find Ncr & Npr
  * 16. Reverse A String In Java
  * 17. Find if a number is palindrome or not
  * 18. Future Investment Value
