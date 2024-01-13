bd8fd8ad8aa2e585e4ffce2366c0613d10536582
AshishKukreti2003 <ashishkukreti4546@gmail.com>
2024-01-13 20:15:46 +0530

    Practice Question Solved

--- Conditionals_and_Loops_Calculator Program/practice/Intermediate_practice.java
+++ Conditionals_and_Loops_Calculator Program/practice/Intermediate_practice.java
@@ -155,36 +155,85 @@ public class Intermediate_practice {
         // System.out.println(result_a);
 
 // 11. Find if a number is palindrome or not
-        System.out.println("Enter the number");
-        int n = sc.nextInt();
-        int result = 0;
-        int org = n;
-        while (n>0) {
-                int rem = n%10;
-                result = result*10+rem;
-                n/=10;   
-        }
+        // System.out.println("Enter the number");
+        // int n = sc.nextInt();
+        // int result = 0;
+        // int org = n;
+        // while (n>0) {
+        //         int rem = n%10;
+        //         result = result*10+rem;
+        //         n/=10;   
+        // }
 
-        if(org == result){
-                System.out.println("Number is palindrome number");
-        }
-        else
-                System.out.println("Number is not palindrome number");
+        // if(org == result){
+        //         System.out.println("Number is palindrome number");
+        // }
+        // else
+        //         System.out.println("Number is not palindrome number");
+
+// 12. HCF Of Two Numbers Program
+        // System.out.print("Enter the 1st number: ");
+        // int a = sc.nextInt();
+        // System.out.print("Enter the 2nd number: ");
+        // int b = sc.nextInt();
+        // int hcf =1;
+        // for(int i=1;i<=a || i<=b;i++){
+        //         if(a%i==0 && b%i ==0)
+        //                 hcf = i;
+        // }
+        // System.out.println(hcf);
+
+
+// 13. LCM Of Two Numbers
+        // System.out.print("Enter the 1st number: ");
+        // int a = sc.nextInt();
+        // System.out.print("Enter the 2nd number: ");
+        // int b = sc.nextInt();
+        // int gcd = 1;
+        // for(int i=1;i<=a || i<=b;i++){
+        //         if(a%i ==0 && b%i ==0){
+        //                 gcd = i;
+        //         }
+        // }
+        // int lcm = a*b/gcd;
+        // System.out.println(lcm);
+
+// 14. Java Program Vowel Or Consonant
+        // System.out.print("Enter the character: ");
+        // char ch = sc.next().charAt(0);
+        // if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
+        //         System.out.println(ch + " is a vowel");
+        // }
+        // else{
+        //         System.out.println(ch+" is consonant");
+        // }
+
+// 15. Perfect Number In Java
+        // System.out.print("Enter the positive number: ");
+        // int a = sc.nextInt();
+        // int count = 1;
+        // int sum =0;
+        // int rem = 1;
+        // while (a>count) {
+        //         if(a%count == 0){
+        //                 sum+=count;
+        //         }
+        //         count++;
+        // }
+        // if(sum == a){
+        //         System.out.println("Perfect number");
+        // }
+        // else
+        //         System.out.println("Not a perfect number");
+
+// 16. Check Leap Year Or Not
         
-
-
         sc.close();
     }
 }
 
 /*
- * 17. 
- * 18. Future Investment Value
- * 19. HCF Of Two Numbers Program
- * 20. LCM Of Two Numbers
- * 21. Java Program Vowel Or Consonant
- * 22. Perfect Number In Java
- * 23. Check Leap Year Or Not
+ * 23. 
  * 24. Sum Of A Digits Of Number
  * 25. Kunal is allowed to go out with his friends only on the even days of a
  * given month. Write a program to count the number of days he can go out in the
