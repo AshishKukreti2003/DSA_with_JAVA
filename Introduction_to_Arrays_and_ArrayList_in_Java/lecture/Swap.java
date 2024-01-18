c6ffc90f7a0e1994971df47bef69dd1b08ddf56f
AshishKukreti2003 <ashishkukreti4546@gmail.com>
2024-01-18 19:21:03 +0530

    Reverse a Array

--- Introduction_to_Arrays_and_ArrayList_in_Java/lecture/Swap.java
+++ Introduction_to_Arrays_and_ArrayList_in_Java/lecture/Swap.java
@@ -3,11 +3,23 @@ import java.util.Arrays;
 public class Swap {
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5};
-        System.out.println("Array: "+Arrays.toString(arr));
-        swap(arr, 0, 4);
-        System.out.print("After Swaping: "+Arrays.toString(arr));
+        // System.out.println("Array: "+Arrays.toString(arr));
+        // swap(arr, 0, 4);
+        // System.out.print("After Swaping: "+Arrays.toString(arr));
+
+        Reverse(arr);
+        System.out.print("Reverse: "+ Arrays.toString(arr));
     }
 
+    public static void Reverse(int[] arr){
+        int start = 0;
+        int end = arr.length-1;
+        while (start<end) {
+            swap(arr, start, end);
+            start++;
+            end--;
+        }
+    }
     public static void swap(int[] arr, int index1, int index2){
         int temp = arr[index1];
         arr[index1] = arr[index2];
