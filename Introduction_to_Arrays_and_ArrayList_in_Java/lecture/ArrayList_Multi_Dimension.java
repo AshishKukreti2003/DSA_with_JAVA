dc8bcfcdd8c9a0aa4cb798721ceac30d13974b47
AshishKukreti2003 <ashishkukreti4546@gmail.com>
2024-01-18 18:51:05 +0530

    ArrayList in MultiDimension

---
+++ Introduction_to_Arrays_and_ArrayList_in_Java/lecture/ArrayList_Multi_Dimension.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class ArrayList_Multi_Dimension {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+
+        // initialization
+        for (int i = 0; i < 3; i++) {
+            list.add(new ArrayList<>());
+        }
+
+        // add elements
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                list.get(i).add(sc.nextInt());
+            }
+        }
+
+        System.out.println(list);
+    }
+}
