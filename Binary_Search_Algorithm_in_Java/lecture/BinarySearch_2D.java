2d9e8942d7d55d9182ca0c701686c607952cffd8
AshishKukreti2003 <ashishkukreti4546@gmail.com>
2024-02-19 19:57:12 +0530

    Binary Search In 2D array

---
+++ Binary_Search_Algorithm_in_Java/lecture/BinarySearch_2D.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+// Matrix is sorted in a row wise and column wise
+public class BinarySearch_2D{
+    public static void main(String[] args) {
+        int[][] arr =  {
+            {10,20,30,40},
+            {12,22,32,42},
+            {14,24,34,44},
+            {16,26,36,46}
+        };
+
+        System.out.println(Arrays.toString(binarySearch(arr, 36)));
+    }
+
+    static int[] binarySearch(int[][] arr, int target){
+        int r = 0;
+        int c = arr.length-1;
+        while(r<arr.length && c>=0){
+            if(arr[r][c] == target){
+                return new int[]{r,c};
+            }
+            else if(arr[r][c] < target){
+                r++;
+            }
+            else{
+                c--;
+            }
+        }
+        return new int[]{-1,-1};
+    }
+}
