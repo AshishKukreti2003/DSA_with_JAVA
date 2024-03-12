2f8f793224a67491c8f3327a68552364e9ee12b9
AshishKukreti2003 <ashishkukreti4546@gmail.com>
2024-03-12 22:09:55 +0530

    Pattern Question in Java

---
+++ Pattern_Question_in_Java/lecture/Main.java
@@ -0,0 +1,72 @@
+public class Main {
+    public static void main(String[] args) {
+        pattern1(5);
+        System.out.println();
+        pattern2(5);
+        System.out.println();
+        pattern3(5);
+        System.out.println();
+        pattern4(5);
+    }
+
+
+/*  1.
+    *****
+    *****
+    *****
+    *****
+    *****
+*/
+    static void pattern1(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+/*  2.
+    *
+    **
+    ***
+    ****
+    *****
+*/
+    static void pattern2(int n){
+        for (int i = 1; i <= n; i++) {
+            for(int j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+/*  3.
+    *****
+    ****
+    ***
+    **
+    *
+*/
+    static void pattern3(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=n;j>=i;j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+/*  4.
+    1
+    1 2
+    1 2 3
+    1 2 3 4
+    1 2 3 4 5
+*/
+    static void pattern4(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j+" ");
+            }System.out.println();
+        }
+    }
+}
