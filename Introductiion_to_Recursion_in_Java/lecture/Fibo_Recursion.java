51ccde8feb383ba8cf89a2d2399c35bed332f4c6
AshishKukreti2003 <ashishkukreti542003@gmail.com>
2024-03-27 13:35:54 +0530

    Fibonacci Formula

--- Introductiion_to_Recursion_in_Java/lecture/Fibo_Recursion.java
+++ Introductiion_to_Recursion_in_Java/lecture/Fibo_Recursion.java
@@ -1,6 +1,13 @@
 public class Fibo_Recursion {
     public static void main(String[] args) {
-        System.out.println(Fibo(4));
+        for(int i=0;i<11;i++){
+
+            System.out.println(FiboFormula(i));
+        }
+    }
+
+    static int FiboFormula(int n){
+        return (int)((Math.pow((1+ Math.sqrt(5))/2, n) + Math.pow((1- Math.sqrt(5))/2, n)) / Math.sqrt(5));
     }
 
     static int Fibo(int n){
