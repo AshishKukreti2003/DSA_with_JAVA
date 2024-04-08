7d1454472f77d7aacde97afae68258615df5f43f
AshishKukreti2003 <ashishkukreti542003@gmail.com>
2024-04-08 12:50:17 +0530

    Factorial using Recursion

---
+++ Introduction_to_Recursion_in_Java/easy/Factorial.java
@@ -0,0 +1,13 @@
+public class Factorial {
+    public static void main(String[] args) {
+        int ans = fact(5);
+        System.out.println(ans);
+    }
+
+    static int fact(int n) {
+        if (n <= 1) {
+            return 1;
+        }
+        return n * fact(n - 1);
+    }
+}
