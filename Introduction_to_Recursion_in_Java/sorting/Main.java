f2ca036d018ba524ef57e3dfb3b883aa52e963b8
AshishKukreti2003 <ashishkukreti4546@gmail.com>
2024-04-11 11:59:20 +0530

    Selection Sort using Recursion

--- Introduction_to_Recursion_in_Java/sorting/Main.java
+++ Introduction_to_Recursion_in_Java/sorting/Main.java
@@ -5,6 +5,10 @@ public class Main{
         int[] arr = {4,2,3,1};
         bubbleSort(arr, arr.length-1, 0);
         System.out.println(Arrays.toString(arr));
+
+        int[] arr1 = {8,4,5,1};
+        selectionSort(arr1, arr1.length, 0, 0);
+        System.out.println(Arrays.toString(arr1));
     }
 
     static void bubbleSort(int[] arr, int r, int c){
@@ -20,6 +24,24 @@ public class Main{
             bubbleSort(arr, r, c+1);
         }else{
             bubbleSort(arr, r-1, 0);
+        }
+    }
+
+    static void selectionSort(int[] arr, int r, int c, int max){
+        if(r == 0){
+            return;
+        }
+        if(c<r){
+            if(arr[c]> arr[max]){
+                selectionSort(arr, r, c+1, c);
+            }else{
+                selectionSort(arr, r, c+1, max);
+            }
+        }else{
+            int temp = arr[max];
+            arr[max] = arr[r-1];
+            arr[r-1] = temp;
+            selectionSort(arr, r-1, 0, 0);
         }
     }
 }
