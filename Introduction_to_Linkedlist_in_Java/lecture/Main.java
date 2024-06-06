478f804c1ed904a5a379b3348522786fd62bac63
AshishKukreti2003 <ashishkukreti4546@gmail.com>
2024-06-06 15:57:26 +0530

    Doubly Linked List in Java

--- Introduction_to_Linkedlist_in_Java/lecture/Main.java
+++ Introduction_to_Linkedlist_in_Java/lecture/Main.java
@@ -1,28 +1,39 @@
 public class Main {
     public static void main(String[] args) {
-        LL list = new LL();
+        // LL list = new LL();
+        // list.insertFirst(3);
+        // list.insertFirst(5);
+        // list.insertFirst(9);
+        // list.insertFirst(4);
+        // list.display();
+        // System.out.println();
+        // list.insertLast(1);
+        // list.display();
+        // System.out.println();
+        // list.insert(30, 3);
+        // list.display();
+        // System.out.println();
+        // System.out.println(list.deleteFirst());
+        // list.display();
+        // System.out.println();
+        // System.out.println(list.deleteLast());
+        // list.display();
+        // System.out.println();
+        // System.out.println(list.delete(2));
+        // list.display();
+        // System.out.println();
+        // list.find(3);
+
+        DLL list = new DLL();
         list.insertFirst(3);
         list.insertFirst(5);
         list.insertFirst(9);
         list.insertFirst(4);
+        list.display();
+        list.insertLast(3);
+        list.display();
+        list.insert(90, 67);
+        list.display();
 
-        list.display();
-        System.out.println();
-        list.insertLast(1);
-        list.display();
-        System.out.println();
-        list.insert(30, 3);
-        list.display();
-        System.out.println();
-        System.out.println(list.deleteFirst());
-        list.display();
-        System.out.println();
-        System.out.println(list.deleteLast());
-        list.display();
-        System.out.println();
-        System.out.println(list.delete(2));
-        list.display();
-        System.out.println();
-        list.find(3);
     }
 }
