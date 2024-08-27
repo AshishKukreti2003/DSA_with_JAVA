952b9a2205b41facd01e137605e9fb8dac3a1327
AshishKukreti2003 <ashishkukreti4546@gmail.com>
2024-08-27 22:26:33 +0530

    Binary Tree in Java

---
+++ Introduction_to_Trees/lecture/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        BinaryTree tree = new BinaryTree();
+        tree.populate(scanner);
+        tree.display(); 
+    }
+}
+ 
