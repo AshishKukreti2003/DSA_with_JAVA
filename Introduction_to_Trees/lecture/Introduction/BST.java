8e043bcd0ce6c3df4358d5fa8e94ec0e978eebef
AshishKukreti2003 <ashishkukreti4546@gmail.com>
2024-09-04 22:37:03 +0530

    BST update

--- Introduction_to_Trees/lecture/Introduction/BST.java
+++ Introduction_to_Trees/lecture/Introduction/BST.java
@@ -20,7 +20,10 @@ class BST {
 
     }
 
-    public int height(Node node){
+    public int height() {
+        return height(root);
+      }
+    private int height(Node node){
         if(node == null) return -1;
 
         return node.height;
